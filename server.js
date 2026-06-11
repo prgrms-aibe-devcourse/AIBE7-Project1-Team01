39e095d6d302a4f55722120b18d0beb73de5d7ba
sangjinlee <dltkdwls0905@gamil.com>
2026-06-11 10:42:28 +0900

    Add social login buttons to login page

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const { createClient } = require("@supabase/supabase-js");
-require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
